9645f522a54067413ffcdd882a603e59186d5621
DaveedGangi <daveedgangi@gmail.com>
2024-03-31 17:38:28 +0000

    NxtTrendz

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import ProductItemDetails from "./components/ProductItemDetails";
+import Cart from "./components/Cart";
+import NotFound from "./components/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import CartContext from "./context/CartContext";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    cartList: [],
+    basedOnPaymentMethod: false,
+  };
+
+  addCartItem = (product) => {
+    const { cartList } = this.state;
+
+    const findingBasedOnId = cartList.find(
+      (eachCart) => eachCart.id === product.id
+    );
+    console.log(findingBasedOnId);
+    if (findingBasedOnId) {
+      this.setState((prevState) => ({
+        cartList: prevState.cartList.map((each) => {
+          if (findingBasedOnId.id === each.id) {
+            const quantityProduct = each.quantity + 1;
+            return { ...each, quantity: quantityProduct };
+          }
+          return each;
+        }),
+      }));
+    } else {
+      this.setState((prevState) => ({
+        cartList: [...prevState.cartList, product],
+      }));
+    }
+  };
+
+  removeCartItem = (id) => {
+    const { cartList } = this.state;
+    const removing = cartList.filter((each) => each.id !== id);
+    this.setState({ cartList: removing });
+  };
+
+  decrementCartItemQuantity = (id) => {
+    const { cartList } = this.state;
+    const quantityDecreased = cartList.find((each) => each.id === id);
+
+    if (quantityDecreased.quantity > 1) {
+      this.setState((prevState) => ({
+        cartList: prevState.cartList.map((each) => {
+          if (each.id === id) {
+            const decreasing = each.quantity - 1;
+            return { ...each, quantity: decreasing };
+          }
+          return each;
+        }),
+      }));
+    } else {
+      this.removeCartItem(id);
+    }
+  };
+
+  incrementCartItemQuantity = (id) => {
+    this.setState((prevState) => ({
+      cartList: prevState.cartList.map((each) => {
+        if (each.id === id) {
+          const increaseQuantity = each.quantity + 1;
+          return { ...each, quantity: increaseQuantity };
+        }
+        return each;
+      }),
+    }));
+  };
+
+  removeAllCartItems = () => {
+    this.setState({ cartList: [] });
+  };
+
+  changeBasedOnPaymentMethod = () => {
+    this.setState({ basedOnPaymentMethod: true });
+  };
+
+  changeBasedOnOtherInputs = () => {
+    this.setState({ basedOnPaymentMethod: false });
+  };
+
+  render() {
+    const {
+      cartList,
+      basedOnPaymentMethod,
+      changeBasedOnPaymentMethod,
+      changeBasedOnOtherInputs,
+    } = this.state;
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+          decrementCartItemQuantity: this.decrementCartItemQuantity,
+          incrementCartItemQuantity: this.incrementCartItemQuantity,
+          removeAllCartItems: this.removeAllCartItems,
+          basedOnPaymentMethod,
+          changeBasedOnPaymentMethod: this.changeBasedOnPaymentMethod,
+          changeBasedOnOtherInputs: this.changeBasedOnOtherInputs,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CartContext.Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Cart/index.js
@@ -0,0 +1,182 @@
+import Popup from "reactjs-popup";
+
+import Header from "../Header";
+import CartListView from "../CartListView";
+
+import CartContext from "../../context/CartContext";
+import EmptyCartView from "../EmptyCartView";
+
+import "./index.css";
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {(value) => {
+      const {
+        cartList,
+        removeAllCartItems,
+        changeBasedOnPaymentMethod,
+        basedOnPaymentMethod,
+        changeBasedOnOtherInputs,
+      } = value;
+      const showEmptyView = cartList.length === 0;
+      let Amount = 0;
+      cartList.forEach((each) => {
+        Amount += each.price * each.quantity;
+      });
+
+      const changePayments = (event) => {
+        console.log(event.target.value);
+        changeBasedOnPaymentMethod();
+      };
+      console.log(basedOnPaymentMethod);
+
+      const changePaymentsByOtherMethod = () => {
+        changeBasedOnOtherInputs();
+      };
+
+      const LengthOfCarts = cartList.length;
+
+      const removingAllCartItems = () => {
+        removeAllCartItems();
+      };
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <div className="changeToRightRemoveAllButton">
+                  <button
+                    onClick={removingAllCartItems}
+                    className="romoveAllButtonStyle"
+                    type="button"
+                  >
+                    Remove All
+                  </button>
+                </div>
+
+                <CartListView />
+                {/* TODO: Add your code for Cart Summary here */}
+
+                <div className="OrderTotalAmount">
+                  <div className="totalAmount">
+                    <h1 className="h1">
+                      <span className="oderTotal">Order Total: </span> Rs{" "}
+                      {Amount}/-
+                    </h1>
+                    <p className="p">{LengthOfCarts} Items in cart</p>
+
+                    <div className="bgForPopup">
+                      <Popup
+                        trigger={
+                          <button className="CheckOut" type="button">
+                            Checkout
+                          </button>
+                        }
+                        modal
+                      >
+                        {(close) => (
+                          <div className="PopUpCard">
+                            <h1>Summery</h1>
+                            <div>
+                              Card, Net Banking, UPI, Wallet, and Cash on
+                              Delivery
+                              <br />
+                              <input
+                                onClick={changePaymentsByOtherMethod}
+                                name="MoneySend"
+                                id="Card"
+                                type="radio"
+                              />
+                              <label htmlFor="Card">Card</label>
+                              <br />
+                              <input
+                                onClick={changePaymentsByOtherMethod}
+                                name="MoneySend"
+                                id="NetBanking"
+                                type="radio"
+                              />
+                              <label htmlFor="NetBanking">Net Banking</label>
+                              <br />
+                              <input
+                                onClick={changePaymentsByOtherMethod}
+                                name="MoneySend"
+                                id="UPI"
+                                type="radio"
+                              />
+                              <label htmlFor="UPI">UPI</label>
+                              <br />
+                              <input
+                                onClick={changePaymentsByOtherMethod}
+                                name="MoneySend"
+                                id="Wallet"
+                                type="radio"
+                              />
+                              <label htmlFor="Wallet">Wallet</label>
+                              <br />
+                              <input
+                                onClick={changePayments}
+                                name="MoneySend"
+                                id="CashOnDelivery"
+                                type="radio"
+                              />
+                              <label htmlFor="CashOnDelivery">
+                                Cash on Delivery
+                              </label>
+                              <br />
+                            </div>
+                            <p>No of items: {LengthOfCarts}</p>
+                            <p>Total price: {Amount}/-</p>
+
+                            <div className="ButtonForEnableAndDisable">
+                              <Popup
+                                trigger={
+                                  <button
+                                    type="button"
+                                    className={
+                                      basedOnPaymentMethod === true
+                                        ? "enable"
+                                        : "disable"
+                                    }
+                                  >
+                                    Confirm Order
+                                  </button>
+                                }
+                                position="top center"
+                                nested
+                              >
+                                <div className="OrderPlaced">
+                                  <p>Your order has been placed successfully</p>
+                                </div>
+                              </Popup>
+                            </div>
+                            <div className="closeButtonFlex">
+                              <button
+                                className="CloseButton"
+                                type="button"
+                                onClick={() => {
+                                  close();
+                                }}
+                              >
+                                close
+                              </button>
+                            </div>
+                          </div>
+                        )}
+                      </Popup>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+      );
+    }}
+  </CartContext.Consumer>
+);
+export default Cart;
